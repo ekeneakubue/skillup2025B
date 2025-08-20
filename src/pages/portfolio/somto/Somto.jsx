d146e3691a54093adf2d751958916197471659df
danielokolo214 <danielokolo214@gmail.com>
2025-08-20 11:54:29 +0100

    Somto-Portfolio

--- src/pages/portfolio/somto/Somto.jsx
+++ src/pages/portfolio/somto/Somto.jsx
@@ -5,45 +5,36 @@ import { Link } from 'react-router-dom'
 
 export default function Somto() {
   return (
-    /*<div>
-      
-        <div className={style.header_container}>
-                    <div className={style.brand}>MY PORTFOLIO</div>
-                    <div className={style.navs}>
-                        <Link to = '/home'>
-                            <div className={style.nav_item}>HOME</div>
-                        </Link>
-                        <Link to = '/skills'>
-                            <div className={style.nav_item}>SKILLS</div>
-                        </Link>
-                        <Link to = '/experience'>
-                            <div className={style.nav_item}>EXPERIENCE</div>
-                        </Link>
-                        <Link to = '/about'>
-                            <div className={style.nav_item}>ABOUT</div>
-                        </Link>
-                        <Link to = '/contact'>
-                            <div className={style.nav_item}>CONTACT</div>
-                        </Link>                
-                    </div>
-                </div>
-
-    </div>*/
+    <>
+    
+    <div className={style.container_somtonavbar}>
+          <div className={style.brand}>Portfolio</div>
+              <ul className={style.navs}>
+                <li>Home</li>
+                <li>About Me</li>
+                <li>Projects</li>
+                <li>Skills</li>
+                <li>Experience</li>
+                <li>Contact</li>
+              </ul>
+              <div className={style.nav_connect}>Connect With Me</div>
+         </div>
 
 
-        <div className={style.container_somtonavbar}>
-          <ul>
-            li*5
-            
-
-            <li className={style.brand}>UNN ICT SkillUp</li>
-          </ul>
-        </div>
+          <div className={style.intro_section}>
+            <img src="/images/portfolios/Elonmusk.jpeg" alt="Somto" className={style.somtoprofile_image} />
+                <h1>Hello, I'm Okolo Somtochukwu, Junior Frontend Developer based in Nigeria. </h1>
+                  <p> I’m a passionate Junior Frontend Developer who loves creating clean, modern, and responsive websites.</p>
+                  <p>Scroll down to explore my projects, skills, and experience 🚀</p>
+          </div>
 
 
 
-
-
+    
+    
+    
+    
+    </>
 
 
   )
